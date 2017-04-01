b3240801160cb0822dfbbcc7bf0640afb2127ae6
regis44 <regis.boullard@gmail.com>
2017-04-01 14:42:21 +0200

    Update writeread.ino

--- spieeprom/examples/writeread/writeread.ino
+++ spieeprom/examples/writeread/writeread.ino
@@ -1,12 +1,12 @@
 #include <spieeprom.h>
 
-SPIEEPROM eeprom(0);// parameter is type
-                    // type=0: 16-bits address
-                    // type=1: 24-bits address
-                    // type>1: defaults to type 0
+SPIEEPROM eeprom(0); // parameter is type
+                     // type = 0: 16-bits address
+                     // type = 1: 24-bits address
+                     // type > 1: defaults to type 0
 
-double testWrite[10] = {-12.59, -10.30, 2.45, 1287454, 9873.10, 34298, 5.63, 247, -128, -47943};
-double testRead[10];
+float testWrite[8] = {1, 2, 3, 4, 5, 6, 7, 8};
+float testRead[8];
 
 void setup() {
   Serial.begin(115200);
@@ -15,13 +15,19 @@ void setup() {
 }
 
 void loop() {
-  eeprom.write(0, testWrite);
-  eeprom.read(0, testRead);
-  for (int i = 0; i < 10; i++) 
+  long address = 0;
+  eeprom.write(address, testWrite);
+  eeprom.read(address, testRead);
+  for (int i = 0; i < 8; i++)
   {
     Serial.print(testRead[i]);
     Serial.print(" ");
   }
   Serial.println();
-  delay(10);
+  address += 8;
+  for (int i = 0; i < 8; i++)
+  {
+    testWrite[i] += 8;
+  }
+  delay(200);
 }
